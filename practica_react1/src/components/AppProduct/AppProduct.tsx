63eb3c03979594cc28f136229d7309e0075d7d80
Ismael Carrillo <iscarrillo65@gmail.com>
2024-10-01 11:49:55 -0300

    Actualizacion de react

--- practica_react1/src/components/AppProduct/AppProduct.tsx
+++ practica_react1/src/components/AppProduct/AppProduct.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import { FromProduct } from "./FromProduct/FromProduct";
+import { Header } from "./Header/Header";
+import { ListProducts } from "./ListProducts/ListProducts";
 
+interface ItemProduct {
+   precio: number;
+   imagen: string;
+   nombre: string;
+}
 
-export const AppProduct = () =>{
+export const AppProduct = () => {
+   const [products, setProducts] = useState<ItemProduct[]>([]);
+
+   console.log(products);
+
+   const handleAddProduct = (newItem: ItemProduct) => {
+      setProducts((prev) => [...prev, newItem]);
+   };
    return (
-   <div>
-      <header />
-   </div>
+      <div>
+         <Header />
+         <h2 className="text-center">Formulario</h2>
+         <FromProduct handleAddProduct={handleAddProduct} />
+         {products.length > 0 ? (
+            <ListProducts arrItems={products} />
+         ) : (
+            <h3 className="text-center">No hay Productos</h3>
+         )}
+      </div>
    );
 };
--- practica_react1/src/components/AppProduct/FromProduct/FromProduct.tsx
+++ practica_react1/src/components/AppProduct/FromProduct/FromProduct.tsx
@@ -1,14 +1,59 @@
-import { Form } from "react-bootstrap"
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "../../hooks/useForm";
+import { FC } from "react";
 
-export const FromProduct = () =>{
-   return(
-      <Form className="p-4 border rounded m-3">
-         
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-
-      </Form>
-   )
+interface IPropsFormProduct {
+   handleAddProduct: Function;
 }
+
+export const FromProduct: FC<IPropsFormProduct> = ({handleAddProduct}) => {
+   const {handleChange, values, resetForm} = useForm({
+      nombre: "",
+      imagen: "",
+      precio: 0,
+   })
+   console.log(values)
+   const handleSubmitForm = () => {
+      handleAddProduct(values);
+      resetForm();
+   }
+
+
+   return (
+      <Form className="p-4 border rounded m-3">
+         <Form.Group className="mb-3" controlId="formNombre">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control
+               type="text"
+               name="nombre"
+               placeholder="Ingrese nombre del producto"
+               value={values.nombre}
+               onChange={handleChange}
+            />
+         </Form.Group>
+         <Form.Group className="mb-3" controlId="formNombre">
+            <Form.Label>Imagen</Form.Label>
+            <Form.Control
+               type="text"
+               name="imagen"
+               placeholder="Ingrese imagen del producto"
+               value={values.imagen}
+               onChange={handleChange}
+            />
+         </Form.Group>
+         <Form.Group className="mb-3" controlId="formNombre">
+            <Form.Label>Precio</Form.Label>
+            <Form.Control
+               type="number"
+               name="precio"
+               placeholder="Ingrese el precio del producto"
+               value={values.precio}
+               onChange={handleChange}
+            />
+         </Form.Group>
+         <div className="d-flex justifi-contet-center mt-4">
+            <Button onClick={handleSubmitForm} variant="primary">Enviar Producto</Button>
+         </div>
+      </Form>
+   );
+};
